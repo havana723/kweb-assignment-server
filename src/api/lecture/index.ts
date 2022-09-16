0a9e0f0ecd0da651e42babcde48ccfd944dc4368
havana723 <71638185+havana723@users.noreply.github.com>
2022-09-17 03:23:10 +0900

    feat: recdents

--- src/api/lecture/index.ts
+++ src/api/lecture/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import get from "./get";
 import post from "./post";
+import recents from "./recents";
 
 const router = Router();
 
 router.get("", get);
 router.post("", post);
+router.get("/recents", recents);
 
 export default router;
---
+++ src/api/lecture/recents.ts
@@ -0,0 +1,51 @@
+import { PrismaClient } from "@prisma/client";
+import { RequestHandler } from "express";
+import { ErrorResponse } from "src/types/Error";
+import { toLectureResponse } from "src/types/LectureResponse";
+
+const get: RequestHandler = async (req, res, next) => {
+  try {
+    if (!req.user) {
+      res.status(401).send({
+        error: "Unauthorized.",
+      } as ErrorResponse);
+      return;
+    }
+
+    if (req.user.role !== "STUDENT") {
+      res.status(401).send({
+        error: "Permission Denied.",
+      } as ErrorResponse);
+      return;
+    }
+
+    const prisma = new PrismaClient();
+
+    const userCourses = await prisma.userCourse.findMany({
+      where: { studentId: req.user.id },
+    });
+    const courseIds = userCourses.map((c) => c.id);
+
+    const lectureCourses = await prisma.lecture.findMany({
+      where: { courseId: { in: courseIds } },
+      include: {
+        course: true,
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+
+    const lectureResponses = Promise.all(
+      lectureCourses.map((l) =>
+        toLectureResponse(l, l.course.courseId, l.course.courseName)
+      )
+    );
+
+    res.status(200).send(lectureResponses);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export default get;
