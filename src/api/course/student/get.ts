43a29aa4870ac1793f1d3d6c6ad09da0c4481ac1
havana723 <71638185+havana723@users.noreply.github.com>
2022-09-17 19:21:23 +0900

    feat: student list

--- src/api/course/student/get.ts
+++ src/api/course/student/get.ts
@@ -1,7 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 import { RequestHandler } from "express";
-import CourseResponse, { toCourseResponse } from "src/types/CourseResponse";
 import { ErrorResponse } from "src/types/Error";
+import { toUserResponse } from "src/types/UserResponse";
 
 const get: RequestHandler = async (req, res, next) => {
   try {
@@ -12,14 +12,39 @@ const get: RequestHandler = async (req, res, next) => {
       return;
     }
 
-    const prisma = new PrismaClient();
-    const courses = await prisma.course.findMany();
+    const courseId = req.query.courseId;
 
-    const courseResponses: CourseResponse[] = await Promise.all(
-      courses.map((course) => toCourseResponse(course))
+    if (!courseId || typeof courseId !== "string") {
+      res.status(400).send({ error: "Invalid courseId." } as ErrorResponse);
+      return;
+    }
+
+    const prisma = new PrismaClient();
+
+    const course = await prisma.course.findUnique({
+      where: { courseId: courseId },
+    });
+
+    if (!course) {
+      res.status(404).send({ error: "Course not Found." } as ErrorResponse);
+      return;
+    }
+
+    if (req.user.id !== course.professorId) {
+      res.status(403).send({ error: "Permission Denied." } as ErrorResponse);
+      return;
+    }
+
+    const userCourses = await prisma.userCourse.findMany({
+      where: { courseId: course.id },
+      include: { student: true },
+    });
+
+    const students = await Promise.all(
+      userCourses.map((u) => toUserResponse(u.student))
     );
 
-    res.status(200).send(courseResponses);
+    res.status(200).send(students);
   } catch (err) {
     next(err);
   }
