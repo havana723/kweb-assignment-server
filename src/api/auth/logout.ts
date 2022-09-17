f130e705f5ad291bb6fcef0c673ae28fefbc905d
havana723 <71638185+havana723@users.noreply.github.com>
2022-09-17 23:05:30 +0900

    fix: logout status code

--- src/api/auth/logout.ts
+++ src/api/auth/logout.ts
@@ -2,7 +2,7 @@ import { RequestHandler } from "express";
 
 const post: RequestHandler = async (req, res) => {
   res.clearCookie("token");
-  res.sendStatus(201);
+  res.sendStatus(200);
 };
 
 export default post;
